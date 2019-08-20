9c3f35621335e1c193afed9d45377e1f25e68fa4
lookpearsw <supajeew@gmail.com>
2019-08-20 14:41:15 +0700

    Create a binding object in the main activity

--- app/src/main/java/com/gmail/supajeew/aboutme/MainActivity.kt
+++ app/src/main/java/com/gmail/supajeew/aboutme/MainActivity.kt
@@ -8,12 +8,15 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.gmail.supajeew.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         findViewById<Button>(R.id.done_button).setOnClickListener {
             addNickname(it)
         }
